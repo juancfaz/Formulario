e2cd59b43f6eb6b068ac47b303c437350b63d098
Juan Faz <jfazleal@icloud.com>
2022-09-13 12:08:09 -0700

    Formulario con base de datos terminado

--- db.php
+++ db.php
@@ -1,13 +1,5 @@
-<?<php>
-//Configuracion necesaria para acceder a la base de datos
-function conn(){
-    $hostname = "localhost";
-    $usuariodb = "root";
-    $passworddb = "";
-    $dbname = "ppi";
+<?php
 
-    // Generando la conexion con el servidor
-    $conectar = mysqli_connect($hostname, $usuariodb, $passworddb, $dbname);
-    return $conectar;
-}
-</php>
+$conexion=mysqli_connect("localhost","root","","Formulario");
+
+?>
--- registro.php
+++ registro.php
@@ -1,13 +1,75 @@
-<?<php>
-//Recibo los datos del formulario
-$nombres = $_POST['nombres'];
-$apellidos = $_POST['apellidos'];
-$direccion = $_POST['direccion'];
-$telefono = $_POST['telefono'];
-$usuario = $_POST['usuario'];
-$clave = $_POST['clave'];
+<?php
+include('db.php');
+$nombre=$_POST['nombre'];
+$apellido=$_POST['apellido'];
+$edad=$_POST['edad'];
+$sexo=$_POST['sexo'];
+$correo=$_POST['correo'];
+$telefono=$_POST['telefono'];
+session_start();
 
-echo "Los datos son los siguientes: <br>";
-echo "$nombres,$apellidos,$direccion,$telefono,$usuario,$clave";
+$conexion=mysqli_connect("localhost","root","","Formulario");
 
-</php>
+$consulta="INSERT INTO registro VALUES(null, '$nombre','$apellido','$edad','$sexo','$correo','$telefono')";
+$resultado=mysqli_query($conexion,$consulta);
+
+if(!$resultado)
+{
+    ?>
+    <h3 class="bad">¡Ups, ha ocurrido un error!</h3>
+    <?php
+}
+else
+{
+    ?>
+    <h3 class="good">¡Conexion exitosa!</h3>
+    <div class="tabla">
+			<table>
+				<tr>
+					<th>ID</th>
+					<th>Nombre</th>
+					<th>Apellido</th>
+					<th>Edad</th>
+					<th>Sexo</th>
+					<th>Correo</th>
+					<th>Telefono</th>
+				</tr>
+					<?php
+						$consulta2 = "SELECT * FROM registro";
+						$ejecutarConsulta = mysqli_query($conexion, $consulta2);
+						$verFilas = mysqli_num_rows($ejecutarConsulta);
+						$fila = mysqli_fetch_array($ejecutarConsulta);
+
+						if(!$ejecutarConsulta){
+							echo"Error en la consulta";
+						}else{
+							if($verFilas<1){
+								echo"<tr><td>Sin registros</td></tr>";
+							}else{
+								for($i=0; $i<=$fila; $i++){
+									echo'
+										<tr>
+											<td>'.$fila[0]. '</td>
+											<td>'.$fila[1]. '</td>
+											<td>'.$fila[2]. '</td>
+											<td>'.$fila[3]. '</td>
+											<td>'.$fila[4]. '</td>
+											<td>'.$fila[5]. '</td>
+											<td>'.$fila[6]. '</td>
+										</tr>
+									';
+									$fila = mysqli_fetch_array($ejecutarConsulta);
+								}
+
+							}
+						}
+					?>
+			</table>
+		</div>
+        <?php
+}
+
+mysqli_free_result($resultado);
+mysqli_close($conexion);
+
+?>
